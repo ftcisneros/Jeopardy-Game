f7499fb67a1f3c10d259cf00c4a1b750901f7f08
Tri4847 <156964222+Tri4847@users.noreply.github.com>
2024-11-05 20:07:52 -0500

    login feature

---
+++ login-submit.php
@@ -0,0 +1,39 @@
+<body>
+    <?php
+    session_start();
+    $csv_file = file_get_contents('registered.txt');
+    $file = fopen("php://temp", 'r+');
+    fputs($file,$csv_file);
+    rewind($file);
+    $users = [];
+
+    while(($data = fgetcsv($file)) !== FALSE){
+        $users[] = $data;
+    }
+    
+    $loginFailed = TRUE;
+
+    for($i = 0; $i < count($users); $i++){
+        if(!isset($users[$i][0])){
+            continue;
+        }
+        else{
+            if($_POST['Username'] == $users[$i][0] && $_POST['Password'] == $users[$i][1]){
+                $_SESSION["Points"] = $users[$i][2];
+                $loginFailed = FALSE;
+            }
+        }
+    }
+    print_r($users);
+    if($loginFailed == TRUE){
+        $_SESSION['loginFail'] = TRUE;
+        $_SESSION['errorMessage'] = 'Invalid username or password.';
+        header("Location: login.php");
+        
+    }
+    else{
+
+        header("Location: test.php"); //page to go to when successful login
+    }
+    ?>
+    </body>
