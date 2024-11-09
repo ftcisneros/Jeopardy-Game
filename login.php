fdf038e5c6a5d61323797c7909e5c7ed50d8a15e
ftcisneros <ftcisneros@gmail.com>
2024-11-09 17:38:51 -0500

    updated login page styling

--- login.php
+++ login.php
@@ -22,13 +22,13 @@
 ?>
 <form action = "login-submit.php" method = "POST">
     <fieldset>
-        <h1>Sign in!</h1>
+        <h1>Login</h1>
         <label for = "username"><strong>Username:</strong></label>
         <input type = "text" placeholder="Username" size = "16" name = "Username" required></br>
         <label for = "password"><strong>Password:</strong></label>
         <input type = "password" placeholder="Password" size = "16" name = "Password" required></br>
         <input type = "submit" name="Submit" value = "Login"/>
-        <p>Don't have an account? <a href = "register.php">Register Now!</a></p>
+        <p>Don't have an account? <a href = "register.php">Register here!</a></p>
     </fieldset>
             
 </form>
