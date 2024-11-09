4e04bff8bef1996f69dac9bd82497a8ec7916643
ftcisneros <ftcisneros@gmail.com>
2024-11-09 17:57:37 -0500

    added styling to registration page

--- register.php
+++ register.php
@@ -1,14 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css"> 
+</head>
+<body>
+
 <?php
     session_start();
 ?>
 <form action = "register-submit.php" method = "POST">
     <fieldset>
-        <h1>Register!</h1>
+        <h1>Register</h1>
         <label for = "username"><strong>Username:</strong></label>
         <input type = "text" size = "16" name = "Username" placeholder="Username" required></br>
         <label for = "password"><strong>Password:</strong></label>
         <input type = "password" size = "16" name = "Password" placeholder="Password" required></br>
-        <input type = "submit" name ="Submit" value = "Register!">
+        <input type = "submit" name ="Submit" value = "Register">
         <p>Already have an account? <a href="login.php">Log in Now!</a></p>
     </fieldset>  
 </form>
+
+</body>
+</html>
